0b8e93e0b2ce3311e645f4ae21be480fd0f30fa5
tuscnj <tuscnj@gmail.com>
2026-06-29 06:28:53 +0000

    Restored Print button for PC

--- resources/views/invoice-preview.blade.php
+++ resources/views/invoice-preview.blade.php
@@ -20,7 +20,7 @@
             </a>
         </div>
         <div class="flex gap-3 w-full sm:w-auto">
-            <button onclick="windows.print();" class="flex-1 sm:flex-none justify-center sm:justify-start bg-slate-800 hover:bg-slate-900 text-white px-5 py-2.5 rounded-lg font-bold shadow-sm transition flex items-center gap-2 text-sm">
+            <button onclick="window.print()" class="flex-1 sm:flex-none justify-center sm:justify-start bg-slate-800 hover:bg-slate-900 text-white px-5 py-2.5 rounded-lg font-bold shadow-sm transition flex items-center gap-2 text-sm">
                 <svg class="w-4 h-4" fill="none" stroke="currentColor" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M17 17h2a2 2 0 002-2v-4a2 2 0 00-2-2H5a2 2 0 00-2 2v4a2 2 0 002 2h2m2 4h6a2 2 0 002-2v-4a2 2 0 00-2-2H9a2 2 0 00-2 2v4a2 2 0 002 2zm8-12V5a2 2 0 00-2-2H9a2 2 0 00-2 2v4h10z"></path></svg>
                 Print
             </button>
